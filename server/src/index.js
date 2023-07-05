abc47a5fba9fc42879b0c6cbd0765fa90b315dfa
tejasveedighe <tejasveedighe@gmail.com>
2023-07-05 22:19:22 +0530

    populate the schema with mock data

--- server/src/index.js
+++ server/src/index.js
@@ -6,10 +6,29 @@ const { makeExecutableSchema } = require("@graphql-tools/schema");
 
 const typeDefs = require("./schema");
 
+const mocks = {
+	Track: () => ({
+		id: () => "track_01",
+		title: () => "Astro Kitty, Space Explorer",
+		author: () => {
+			return {
+				name: "Grumpy Cat",
+				photo:
+					"https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+			};
+		},
+		thumbnail: () =>
+			"https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+		length: () => 1210,
+		modulesCount: () => 6,
+	}),
+};
+
 async function startApolloServer() {
 	const server = new ApolloServer({
 		schema: addMocksToSchema({
 			schema: makeExecutableSchema({ typeDefs }),
+			mocks,
 		}),
 	});
 	const { url } = await startStandaloneServer(server);
