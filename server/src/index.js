5b14c8f14ec983eb24e4616b560f486cc88597d4
tejasveedighe <tejasveedighe@gmail.com>
2023-07-05 22:17:37 +0530

    Executable schema populates the string with Hello World

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
 const typeDefs = require("./schema");
 
 async function startApolloServer() {
-	const server = new ApolloServer({ typeDefs });
+	const server = new ApolloServer({
+		schema: addMocksToSchema({
+			schema: makeExecutableSchema({ typeDefs }),
+		}),
+	});
 	const { url } = await startStandaloneServer(server);
 	console.log(`Server is running at ${url}`);
 }
