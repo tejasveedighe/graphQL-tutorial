0e38bc4895c1c17911e6feefd80715a1ac85e4ab
tejasveedighe <tejasveedighe@gmail.com>
2023-07-05 22:13:22 +0530

    Added apollo server, it does nothing

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,12 @@
-//TODO
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
+const typeDefs = require("./schema");
+
+async function startApolloServer() {
+	const server = new ApolloServer({ typeDefs });
+	const { url } = await startStandaloneServer(server);
+	console.log(`Server is running at ${url}`);
+}
+
+startApolloServer();
