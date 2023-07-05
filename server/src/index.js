910f4df6dee4d23bfc7530dd7c0f22c84c0486f4
tejasveedighe <tejasveedighe@gmail.com>
2023-07-05 22:22:07 +0530

    populate the schema with mock data with 6 objects default is 2

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,9 @@ const { makeExecutableSchema } = require("@graphql-tools/schema");
 const typeDefs = require("./schema");
 
 const mocks = {
+	Query: () => ({
+		tracksForHome: () => [...new Array(6)],
+	}),
 	Track: () => ({
 		id: () => "track_01",
 		title: () => "Astro Kitty, Space Explorer",
