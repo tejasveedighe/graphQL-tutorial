c2405b60928765a7dfadaa6976f70c335e11f5c0
tejasveedighe <tejasveedighe@gmail.com>
2023-07-06 20:32:13 +0530

    added the codegen, i dont know what it did, and also set a basic query in the client

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,11 +1,14 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-	schema: "https://localhost:4000",
+	schema: "http://localhost:4000",
 	documents: ["src/**/*.tsx"],
 	generates: {
 		"./src/__generated__/": {
 			preset: "client",
+			presetConfig: {
+				gqlTagName: "gql",
+			},
 		},
 	},
 };
