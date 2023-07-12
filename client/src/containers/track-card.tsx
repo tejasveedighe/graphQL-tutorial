5fe1a45e460ccd6be6d4ee4f0b53c7cc459bb1ae
tejasveedighe <tejasveedighe@gmail.com>
2023-07-12 20:53:10 +0530

    Used Link instead of navigate

--- client/src/containers/track-card.tsx
+++ client/src/containers/track-card.tsx
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 import { colors, mq } from "../styles";
 import { humanReadableTimeFromSeconds } from "../utils/helpers";
 import type { Track } from "../__generated__/graphql";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 /**
  * Track Card component renders basic info in a card format
@@ -11,12 +11,9 @@ import { useNavigate } from "react-router-dom";
  */
 const TrackCard: React.FC<{ track: Omit<Track, "modules"> }> = ({ track }) => {
 	const { title, thumbnail, author, length, modulesCount, id } = track;
-	const navigate = useNavigate();
-	const handleCardClick = (id: String) => {
-		navigate(`/track/${id}`);
-	};
+
 	return (
-		<CardContainer onClick={() => handleCardClick(id)}>
+		<CardContainer to={`/track/${id}`}>
 			<CardContent>
 				<CardImageContainer>
 					<CardImage src={thumbnail || ""} alt={title} />
@@ -42,34 +39,34 @@ const TrackCard: React.FC<{ track: Omit<Track, "modules"> }> = ({ track }) => {
 export default TrackCard;
 
 /** Track Card styled components */
-const CardContainer = styled.div({
-  borderRadius: 6,
-  color: colors.text,
-  backgroundSize: "cover",
-  backgroundColor: "white",
-  boxShadow: "0px 1px 5px 0px rgba(0,0,0,0.15)",
-  backgroundPosition: "center",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-  [mq[0]]: {
-    width: "90%",
-  },
-  [mq[1]]: {
-    width: "47%",
-  },
-  [mq[2]]: {
-    width: "31%",
-  },
-  height: 380,
-  margin: 10,
-  overflow: "hidden",
-  position: "relative",
-  ":hover": {
-    backgroundColor: colors.pink.lightest,
-  },
-  cursor: "pointer",
-  textDecoration: "none",
+const CardContainer = styled(Link)({
+	borderRadius: 6,
+	color: colors.text,
+	backgroundSize: "cover",
+	backgroundColor: "white",
+	boxShadow: "0px 1px 5px 0px rgba(0,0,0,0.15)",
+	backgroundPosition: "center",
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "space-between",
+	[mq[0]]: {
+		width: "90%",
+	},
+	[mq[1]]: {
+		width: "47%",
+	},
+	[mq[2]]: {
+		width: "31%",
+	},
+	height: 380,
+	margin: 10,
+	overflow: "hidden",
+	position: "relative",
+	":hover": {
+		backgroundColor: colors.pink.lightest,
+	},
+	cursor: "pointer",
+	textDecoration: "none",
 });
 
 const CardContent = styled.div({
